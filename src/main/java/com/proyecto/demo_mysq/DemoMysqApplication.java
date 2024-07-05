e05ca63d07c2d849cdc2f6cd235eae53770756ec
MisheandEli <mishe0706@gmail.com>
2024-07-04 19:29:09 -0500

    spring

---
+++ src/main/java/com/proyecto/demo_mysq/DemoMysqApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.demo_mysq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMysqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMysqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/demo_mysq/DemoMysqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.demo_mysq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMysqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
